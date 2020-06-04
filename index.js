301634a12fc6e7ac87f582225ce17cc62540fead
Stromwerk <k.koichev1314@gmail.com>
2020-06-04 15:50:01 +0300

    Add inverted color to text

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function updateTime() {
     const timeEl = document.getElementById('time');
     const date = new Date().toTimeString().substr(0, 8);
+    const dateColor = `#${date.replace(/\:/g, '')}`;
     timeEl.textContent = date;
-    document.body.style.background = `#${date.replace(/\:/g, '')}`;
+    document.body.style.background = invert(dateColor);
+    document.getElementById('time').style.color = dateColor;
 }
 updateTime();
 setInterval(updateTime, 1000);
